ce9d79b8ab998200f77abf67b63bd0e9b358f3be
Peter Byfield <peter.byfield@kraken.tech>
2025-11-08 21:25:31 +0100

    Stub python implementation

--- python/fastwalk/__init__.py
+++ python/fastwalk/__init__.py
@@ -1,3 +1,63 @@
-import fastwalk._core as core  # ty: ignore[unresolved-import]
+import dataclasses
+import functools
+from collections.abc import Collection, Iterator
+from pathlib import Path
 
-sum_as_string = core.sum_as_string
+from fastwalk import _core  # ty: ignore[unresolved-import]
+
+PathLike = Path | str
+
+
+@dataclasses.dataclass(frozen=True)
+class DirEntry:
+    _core_entry: _core.DirEntry
+
+    @functools.cached_property
+    def path(self) -> Path:
+        # TODO
+        ...
+
+    @functools.cached_property
+    def path_str(self) -> str:
+        # TODO
+        ...
+
+    @functools.cached_property
+    def is_file(self) -> bool:
+        # TODO
+        ...
+
+    @functools.cached_property
+    def is_dir(self) -> bool:
+        # TODO
+        ...
+
+    @functools.cached_property
+    def is_symlink(self) -> bool:
+        # TODO
+        ...
+
+
+def walk(
+    root: PathLike,
+    *,
+    filters: Collection[str] = (),  # Glob patterns ("any of" semantics).
+    ignore_dirs: Collection[PathLike] = (),  # Absolute, or relative to `root`.
+    ignore_hidden: bool = True,
+    respect_git_ignore: bool = True,
+    respect_global_git_ignore: bool = True,
+    respect_git_exclude: bool = True,
+    respect_ignore: bool = True,
+    follow_symlinks: bool = False,
+    max_depth: int | None = None,
+    min_depth: int | None = None,
+    max_filesize: int | None = None,
+    threads: int = 0,
+) -> Iterator[DirEntry]:
+    # TODO
+    # - Implement this function, calling rust via _core.
+    # - Use the `ignore` crate in rust to implement this.
+    # - Use `WalkBuilder.build_parallel` to use multiple threads.
+    # - Use the `crossbeam` crate to create a channel for sending the results back to python.
+    # - Think about correct error handling and optimal performance.
+    ...
--- python/tests/test_fastwalk.py
+++ python/tests/test_fastwalk.py
@@ -1,5 +1,1 @@
 import fastwalk
-
-
-def test_sum_as_string():
-    assert fastwalk.sum_as_string(1, 1) == "2"
