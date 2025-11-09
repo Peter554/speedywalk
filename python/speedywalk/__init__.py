30ba2111c5cbeed3247fb8eecd4c9bec1cc931ea
Peter Byfield <peter.byfield@kraken.tech>
2025-11-09 10:03:55 +0100

    Add badges to module docstring

--- python/speedywalk/__init__.py
+++ python/speedywalk/__init__.py
@@ -1,5 +1,12 @@
 """Fast parallel directory walking for Python, powered by Rust.
 
+[![PyPI](https://img.shields.io/pypi/v/speedywalk.svg)](https://pypi.org/project/speedywalk/)
+[![CI](https://github.com/Peter554/speedywalk/actions/workflows/check.yml/badge.svg)](https://github.com/Peter554/speedywalk/actions/workflows/check.yml)
+[![License: MIT](https://img.shields.io/badge/License-MIT-yellow.svg)](https://opensource.org/licenses/MIT)
+[![Documentation](https://img.shields.io/badge/docs-latest-blue.svg)](https://peter554.github.io/speedywalk/)
+
+Find the repo [here](https://github.com/Peter554/speedywalk).
+
 ## Features
 
 - 🚀 **Fast**: Uses the rust [ignore](https://crates.io/crates/ignore) crate for fast directory traversal.
